ff5ac5aedbb0455d6aa7e6eab0bc3d6c32f7bd1d
Escartin85 <scr85matrix@gmail.com>
2021-04-06 12:13:45 +0100

    Setup All urls and View empties

--- dotfive/accounts/urls.py
+++ dotfive/accounts/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
 
-from accounts import views
+from . import views
 
 urlpatterns = [
-	
+    url(r'^$', views.home_page, name='homePage'),
+    url(r'^login/$', views.login_page, name='loginPage'),
+    url(r'^register/$', views.register_page, name='registerPage'),
+    url(r'^logout/$', views.logout_page, name='logoutPage'),
+	url(r'^account/item/$', views.item_list, name='itemList'),
+    url(r'^account/item/create/$', views.item_create, name='itemCreateNew'),
+    url(r'^account/item/(?P<slug>[\w-]+)/$', views.item_detail, name='itemDetail'),
+    url(r'^account/item/(?P<slug>[\w-]+)/edit/$', views.item_update, name='itemUpdate'),
+    url(r'^account/item/(?P<slug>[\w-]+)/delete/$', views.item_delete, name='itemDelete'),
 ]
--- dotfive/accounts/views.py
+++ dotfive/accounts/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+# View for Home Page
+def home_page(request):
+
+    data = ''
+    context_data = {
+        "formWorker": data,
+        "Title": "Home",
+    }
+
+    #return render(request, "index.html", context_data)
+    return redirect('loginPage')
+
+# View for Login Page
+def login_page(request):
+    data = ''
+    context_data = {
+        "formWorker": data,
+        "Title": "Home",
+    }
+
+    return render(request, "accounts/login.html", context_data)
+
+# View for Register Page
+def register_page(request):
+    data = ''
+    return redirect('loginPage')
+
+# View for Logout Page
+def logout_page(request):
+    data = ''
+    return redirect('loginPage')
+    
+# View for Logout
+def logut_page(request):
+    data = ''
+    return redirect('loginPage')
+
+# View for List all Items
+def item_list(request):
+    data = ''
+    return redirect('loginPage')
+
+# View for Detail an Items
+def item_detail(request):
+    data = ''
+    return redirect('loginPage')
+    
+# View for Create an Items
+def item_create(request):
+    data = ''
+    return redirect('loginPage')
+
+# View for Update an Items
+def item_update(request):
+
+    return redirect('loginPage')
+
+# View for Delete an Items
+def item_delete(request):
+    data = ''
+    return redirect('loginPage')
