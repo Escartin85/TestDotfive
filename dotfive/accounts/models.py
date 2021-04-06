13076175ecd2b9664471ead6ab02d67c855fa283
Escartin85 <scr85matrix@gmail.com>
2021-04-06 13:45:28 +0100

    Added Item Model

--- dotfive/accounts/models.py
+++ dotfive/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# Class Model of Item
+class Item(models.Model):
+	CATEGORY = (
+			('Category1', 'Category 1'),
+			('Category2', 'Category 2'),
+            ('Category3', 'Category 3'),
+			) 
+
+	label = models.CharField(max_length=200, null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.label
