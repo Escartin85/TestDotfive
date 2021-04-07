d9aff2daa47543879d7e6a08cb667d82f39f5889
Escartin85 <scr85matrix@gmail.com>
2021-04-07 17:25:38 +0100

    Some basic test

--- dotfive/accounts/tests.py
+++ dotfive/accounts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+from .models import Item
 # Create your tests here.
+
+# Class for Test the Item Class
+class ItemTest(TestCase):
+    
+    def setUp(self):
+        self.item = Item()
+        self.item.label = "Hello_Test"
+        self.item.slug = "hello_test"
+        self.item.category = "Category1"
+        self.item.save()
+        
+        record = Item.items.get(slug="hello_test")
+        self.assertEqual(record, self.item)
+        
+    def test_slug_on_save(self):
+        self.item = Item()
+        self.item.label = "Hello_Testing"
+        self.item.category = "Category1"
+        self.item.save()
+        
+        self.assertEqual(self.item.slug, "hello_testing")
+        
